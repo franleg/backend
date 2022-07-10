f6f7ac4a5673323d84d492ac0984ae16350467b6
Fran Leg <francolegnazzi@hotmail.com>
2022-07-10 20:46:33 -0300

    Carga proyecto en Express

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import express from "express";
+import { ProductManager } from "./managers/productManager.js";
+
+const app = express();
+
+const productService = new ProductManager();
+
+const PORT = 8080;
+
+let counter = 0;
+
+const server = app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+
+
+app.get('/', (req,res)=>{
+    res.send(`
+                <h1>¡Hello from Express!</h1>
+                <h2>Choose beetween the given paths:</h2>
+                <ul>
+                    <li>/products</li>
+                    <li>/random</li>
+                </ul>
+            `)
+})
+
+app.get('/visits',(re,res)=>{
+    counter++;
+    res.send(`The server has been visited ${counter} times`)
+})
+
+app.get('/products', async (req, res)=>{
+    let products = await productService.getAll();
+    let category = req.query.category;
+    let id = parseInt(req.query.id);
+    console.log(id)
+    if(id){
+        let productsById = products.filter(prod => prod.id === id);
+        console.log(productsById)
+        if(productsById.length === 0){
+            return res.send(`The product with id ${id} does not exist. Please choose another.`)
+        }else {
+            return res.send(productsById)
+        }
+    }    
+    if(category){
+        let productsByCategory = products.filter(prod => prod.category === category);
+        return res.send(productsByCategory)
+    }
+    return res.send(products)
+})
+
+app.get('/random', async (req, res)=>{
+    let products = await productService.getAll();
+    let random = products[Math.floor(Math.random() * products.length)]
+    res.send(random)
+})
+
